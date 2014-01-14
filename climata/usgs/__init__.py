5fbeccfc147face0dfe22093f0651379ae6e810d
Jeremiah Sandahl <jeremiahsa@gmail.com>
2014-01-14 12:50:00 -0600

    Abstracted the USGS code

--- climata/usgs/__init__.py
+++ climata/usgs/__init__.py
@@ -6,53 +6,132 @@ import os
 
 filename = os.path.join(os.path.dirname(__file__), 'parameter_cd_query.txt')
 
+
 class USGSIO(CsvNetIO):
     """
-    Base class for loading data from USGS web services This loads the summary data. 
-    url: http://waterservices.usgs.gov/nwis/site/?format=rdb,1.0&huc=18010101&seriesCatalogOutput=true&outputDataTypeCd=all
+      Base class for loading data from USGS web services: summary.
+      url: http://waterservices.usgs.gov/nwis/site/?format=rdb,
+        1.0&huc=18010101&seriesCatalogOutput=true&outputDataTypeCd=all
     """
-    basin = 18010101 #, 18010102, 18010103, etc.
-    max_header_row=100
+    basin = None  # 18010201  # ,18010202, 18010203, etc.
+    max_header_row = 100
     delimiter = "\t"
 
     @property
+    def params(self):
+        return {
+            'format': 'rdb,1.0',
+            'huc': self.basin,
+            'seriesCatalogOutput': 'true',
+            'outputDataTypeCd': 'all',
+            }
+
+    @property
     def url(self):
-        return "http://waterservices.usgs.gov/nwis/site/?format=rdb,1.0&huc=%s&seriesCatalogOutput=true&outputDataTypeCd=all" % self.basin
-  
-  
+        return "http://waterservices.usgs.gov/nwis/site/" % self.params
+
     def list_to_csv(self):
         pass
 
+
 class SiteDataIO(JsonNetIO):
     """
-    Base class for loading data from the USGS web services. This loads the actual site data. 
+    Base class for loading data from the USGS web services: actual site data.
+
+    Here's how the data is layed out:
+    [{index}]       # The index is for each site
+        [1]         # 0 = sourceInfo; 1 = Values; 2 = Variables; 3 = Name
+            [0]     # This should probably be left as '0' as it's a spacer
+                ['value'] # This is to get the values list for each site
+                    [{index}] # Loop through this level for each site to get
+                              # to get the values
     """
-    site_name = 11532000
-    max_header_row = 100
+    basin = None
+    debug = True
+    namespace = "value.timeSeries"
+    start_date = None  # '1950-01-01'
+    end_date = datetime.strftime(datetime.today(), '%Y-%m-%d')
+    siteType = None  # 'LK,ST,ST-CA,ST-DCH,ST-TS,SP,
+    # GW,GW-CR,GW-EX,GW-HZ,GW-IW,GW-MW,GW-TH'
+    parameterCd = None  # '00055,00056,00058,00059,00060,00061,
+    # 00062,00064,00065,00072,72015,72016,72019,72020'
+
+    @property
+    def params(self):
+        return {
+            'format': 'json',
+            'indent': 'on',
+            'huc': self.basin,
+            'startDT': self.start_date,
+            'endDt': self.end_date,
+            'siteType': self.siteType,  # Not necessary with parameter codes.
+            'parameterCd': self.parameterCd,  # measurements in ft and ft3/s
+        }
+
     @property
     def url(self):
-        return "http://waterservices.usgs.gov/nwis/dv/?format=json&indent=on&sites=%s&startDT=1950-01-01&endDT=2013-12-17" % self.site_name
-    
+        return "http://waterservices.usgs.gov/nwis/dv/"
+
+
+class SitesForHucIO(SiteDataIO):
+    '''
+    This is just designed to obtain all valid sites for a given huc id.
+    '''
+    basin = None  # 18010202
+    debug = False
+    startDT = None  # '2013-09-01'
+    endDT = None  # '2013-12-30'
+    parameterCd = None  # '00055,00056,00058,00059,00060,00061,
+    # 00062,00064,00065,00072,72015,72016,72019,72020'
+
+    @property
+    def url(self):
+        return "http://waterservices.usgs.gov/nwis/iv/"
+
+    @property
+    def params(self):
+        return {
+            'format': 'json,1.1',
+            'huc': self.basin,
+            'startDT': self.startDT,
+            'endDT': self.endDT,
+            'parameterCd': self.parameterCd,
+        }
+
+
+class InstantValuesIO(SiteDataIO):
+    '''
+    This class examines the instantaneous values for USGS data.
+    '''
+
+    start_date = '2013-09-01'
+    sites = '11493500'
+
+    @property
+    def url(self):
+        return "http://waterservices.usgs.gov/nwis/iv/"
+
+    @property
+    def params(self):
+        return {
+            'format': 'json,1.1',
+            'sites': self.sites,
+            'startDT': self.start_date,
+            'endDT': self.end_date,
+            # 'parameterCd':'00055,00056,00058,00059,00060,00061,
+            # 00062,00064,00065,00072,72015,72016,72019,72020',
+            # 'siteType':'LK,ST,ST-CA,ST-DCH,ST-TS,SP,
+            # GW,GW-CR,GW-EX,GW-HZ,GW-IW,GW-MW,GW-TH',
+        }
+
+
 class ParamIO(CsvFileIO):
     """
-    The USGS water data has a very large code mapping segment for the parm_cd field that needs to be put into readable text
+    The USGS water data has a very large code mapping segment for the parm_cd
+    field that needs to be put into readable text
     """
     delimiter = "\t"
-    debug=True
-    max_header_row=8
+    debug = True
+    max_header_row = 8
     filename = filename
     key_field = 'parm_cd'
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
--- climata/usgs/constants.py
+++ climata/usgs/constants.py
@@ -1,7 +1,7 @@
 data_type_cd = {
     'all': 'default',
     'iv': 'instantaneous Values',
-    'ut': 'Unit Values',
+    'uv': 'Unit Values',
     'rt': 'Real Time Values',
     'dv': 'Daily Values',
     'pk': 'Peak Measurements',
@@ -38,7 +38,70 @@ summary_header_fields = (
     'End Date',
     'Number of Records',
     )
+
+site_types = {
+    'AG': 'Aggregate groundwater use',
+    'AS': 'Aggregate surface-water-use',
+    'AT': 'Atmosphere',
+    'AW': 'Aggregate water-use establishment',
+    'ES': 'Estuary',
+    'FA': 'Facility',
+    'FA-AWL': 'Animal waste lagoon',
+    'FA-CI': 'Cistern',
+    'FA-CS': 'Combined sewer',
+    'FA-DV': 'Diversion',
+    'FA-FON': 'Field, Pasture, Orchard, or Nursery',
+    'FA-GC': 'Golf course',
+    'FA-HP': 'Hydroelectric plant',
+    'FA-LF': 'Landfill',
+    'FA-OF': 'Outfall',
+    'FA-PV': 'Pavement',
+    'FA-QC': 'Laboratory or sample-preparation area',
+    'FA-SEW': 'Wastewater sewer',
+    'FA-SPS': 'Septic system',
+    'FA-STS': 'Storm sewer',
+    'FA-TEP': 'Thermoelectric plant',
+    'FA-WDS': 'Water-distribution system',
+    'FA-WIW': 'Waste injection well',
+    'FA-WTP': 'Water-supply treatment plant',
+    'FA-WU': 'Water-use establishment',
+    'FA-WWD': 'Wastewater land application',
+    'FA-WWTP': 'Wastewater-treatment plant',
+    'GL': 'Glacier',
+    'GW': 'Well',
+    'GW-CR': 'Collector or Ranney type well',
+    'GW-EX': 'Extensometer well',
+    'GW-HZ': 'Hyporheic-zone well',
+    'GW-IW': 'Interconnected wells',
+    'GW-MW': 'Multiple wells',
+    'GW-TH': 'Test hole not completed as a well',
+    'LA': 'Land',
+    'LA-EX': 'Excavation',
+    'LA-OU': 'Outcrop',
+    'LA-PLY': 'Playa',
+    'LA-SH': 'Soil hole',
+    'LA-SNK': 'Sinkhole',
+    'LA-SR': 'Shore',
+    'LA-VOL': 'Volcanic vent',
+    'LK': 'Lake, Reservoir, Impoundment',
+    'OC': 'Ocean',
+    'OC-CO': 'Coastal',
+    'SB': 'Subsurface',
+    'SB-CV': 'Cave',
+    'SB-GWD': 'Groundwater drain',
+    'SB-TSM': 'Tunnel, shaft, or mine',
+    'SB-UZ': 'Unsaturated zone',
+    'SP': 'Spring',
+    'SS': 'Specific Source',
+    'ST': 'Stream',
+    'ST-CA': 'Canal',
+    'ST-DCH': 'Ditch',
+    'ST-TS': 'Tidal stream',
+    'WE': 'Wetland',
+}
+
 '''
+ Summary Header Fields
     sites.field_names[0] = 'Agency' #agency_cd
     sites.field_names[1] = 'Site Number' #site_no
     sites.field_names[2] = 'Site Name' #station_nm
@@ -50,13 +113,14 @@ summary_header_fields = (
     sites.field_names[8] = 'Altitude of Gage/land surface' #alt_va
     sites.field_names[9] = 'Altitude Accuracy' #alt_acy_va
     sites.field_names[10] = 'Altitude Datum' #alt_datum_cd
-    sites.field_names[11] = 'Data Type Code' #data_type_cd = This is the data_type_cd field that needs to be swapped out.
-    sites.field_names[12] = 'Parameter Code' #'parm_cd' #USGS parameter code from fixed_parms_query.txt
-    sites.field_names[13] = 'Statistics Code' #stat_cd http://help.waterdata.usgs.gov/stat_code
+    sites.field_names[11] = 'Data Type Code' #data_type_cd
+    sites.field_names[12] = 'Parameter Code' #'parm_cd'
+    sites.field_names[13] = 'Statistics Code' #stat_cd
+    #http://help.waterdata.usgs.gov/stat_code
     ### This following section is part of the period of record data
     sites.field_names[14] = 'dd_nu' #dd_nu No idea what this field means
-    sites.field_names[15] = 'loc_web_ds' #loc_web_ds This field has no data in any of the test data columns
-    sites.field_names[16] = 'Medium Group Code' #medium_grp_cd Don't know what this means.
+    sites.field_names[15] = 'loc_web_ds' #loc_web_ds
+    sites.field_names[16] = 'Medium Group Code' #medium_grp_cd
     sites.field_names[17] = 'Parameter Group Code' #parm_grp_cd
     ### End permit of record data unknowns
     sites.field_names[18] = 'Substance Registry Service ID' #srs_id
@@ -65,5 +129,3 @@ summary_header_fields = (
     sites.field_names[21] = 'End Date' #end_date
     sites.field_names[22] = 'Number of Records' #count_nu
     '''
-    
-    #
