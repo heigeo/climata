9bdae7db069a45401d247340b8614c7a85ce755b
Jeremiah Sandahl <jeremiahsa@gmail.com>
2014-01-20 13:41:38 -0600

    Cleaned up the formatting on snotel.

--- climata/snotel/__init__.py
+++ climata/snotel/__init__.py
@@ -3,12 +3,15 @@ from datetime import datetime, date, timedelta
 from wq.io.parsers import readers
 from collections import namedtuple, OrderedDict
 from SOAPpy import SOAPProxy
-import os, time, pickle
+import os
+import time
+import pickle
 
 namespace = 'http://www.wcc.nrcs.usda.gov/ns/awdbWebService'
 url = 'http://www.wcc.nrcs.usda.gov/awdbWebService/services?WSDL'
 server = SOAPProxy(url, namespace)
 today = datetime.now()
+
 
 class SnotelIO(BaseIO):
     '''
@@ -16,10 +19,10 @@ class SnotelIO(BaseIO):
      The data is stored in a cached file to speed loading, then
      it is loaded from the cache and returned.
     '''
-    filename = None # File name is the actual filename 
+    filename = None  # File name is the actual filename
     data_function = None
     cache = True
-    
+
     def load(self):
         if self.cache:
             if self.checkfile():
@@ -29,13 +32,13 @@ class SnotelIO(BaseIO):
             f.close()
         else:
             self.data = self.data_function
-        
+
     def write_cache(self):
         f = open(self.filename, 'w+')
         data = self.data_function
         pickle.dump(data, f)
         f.close()
-    
+
     def checkfile(self):
         if os.path.isfile(self.filename):
             filedate = datetime.fromtimestamp(os.path.getmtime(self.filename))
@@ -46,17 +49,17 @@ class SnotelIO(BaseIO):
                 return False
         else:
             return False
-        
+
 
 class SnotelSiteListIO(SnotelIO):
     hucs = None
     networkCds = None
     cache = True
-    
+
     @property
     def filename(self):
         return 'cache/site_list_%s.py' % self.hucs.replace(':', '-')
-    
+
     @property
     def data_function(self):
         return server.getStations(
@@ -69,44 +72,47 @@ class SnotelSiteListIO(SnotelIO):
 class SnotelStationMetaIO(SnotelIO):
     site = None
     cache = True
-    
+
     @property
     def filename(self):
         return 'cache/station_meta_%s.py' % self.site.replace(':', '-')
-    
+
     @property
     def data_function(self):
         return server.getStationMetadata(stationTriplet=self.site)
-    
+
+
 class SnotelStationElementsIO(SnotelIO):
     station = None
     cache = True
-    
+
     @property
     def filename(self):
         return 'cache/station_elements_%s.py' % self.station.replace(':', '-')
-    
+
     @property
     def data_function(self):
         return server.getStationElements(stationTriplet=str(self.station))
-    
+
+
 class SnotelElementsIO(SnotelIO):
     cache = False
-    
+
     @property
     def data_function(self):
         return server.getElements()
-    
+
+
 class SnotelDailyDataIO(SnotelIO):
     station_triplets = None
     element_cd = None
     start_date = None
-    
+
     @property
     def filename(self):
-        return 'cache/daily_data_%s_%s.py' % (self.station_triplets.replace(':', '-'),
-                                              self.element_cd)
-    
+        return 'cache/daily_data_%s_%s.py' % (
+            self.station_triplets.replace(':', '-'), self.element_cd)
+
     @property
     def data_function(self):
         return server.getData(
@@ -114,32 +120,33 @@ class SnotelDailyDataIO(SnotelIO):
             elementCd=self.element_cd,
             ordinal=1,
             duration='DAILY',
-            getFlags='true',  # Needs to be true for the other to be false...goofy!
+            getFlags='true',  # Needs to be true for the other to be false
             beginDate=self.start_date,
             endDate=str(datetime.date(datetime.now()).isoformat()),
             alwaysReturnDailyFeb29='false'
             )
-    
+
+
 class SnotelHourlyDataIO(SnotelIO):
     station_triplets = None
     element_cd = None
     start_date = None
-    
+
     @property
     def filename(self):
-        return 'cache/hourly_data_%s_%s.py' % (self.station_triplets.replace(':', '-'),
-                                               self.element_cd)
-    
+        return 'cache/hourly_data_%s_%s.py' % (
+            self.station_triplets.replace(':', '-'), self.element_cd)
+
     @property
     def data_function(self):
         return server.getHourlyData(
-            stationTriplets = self.station_triplets,
-            elementCd = self.element_cd,
-            ordinal = 1,
-            beginDate = self.start_date,
-            endDate = str(datetime.date(datetime.now()).isoformat()),
+            stationTriplets=self.station_triplets,
+            elementCd=self.element_cd,
+            ordinal=1,
+            beginDate=self.start_date,
+            endDate=str(datetime.date(datetime.now()).isoformat()),
         )
-    
+
 
 def checkfile(filename=None):
     '''
@@ -156,14 +163,17 @@ def checkfile(filename=None):
     else:
         return True
 
-def site_list(hucs = None, networkCds=None):
+
+def site_list(hucs=None, networkCds=None):
     '''
      This function returns a list of sites in triplet format
      Triplets have this format: [station id]:[state code]:[network code]
      http://www.wcc.nrcs.usda.gov/web_service/AWDB_Web_Service_Reference.htm
      Network Codes:
-     BOR Any Bureau of Reclamation reservoir stations plus other non-BOR reservoir stations
-     CLMIND Used to store climate indices (such as Southern Oscillation Index or Trans-Nino Index)
+     BOR Any Bureau of Reclamation reservoir stations plus
+                    other non-BOR reservoir stations
+     CLMIND Used to store climate indices (such as Southern Oscillation Index
+                    or Trans-Nino Index)
      COOP National Weather Service COOP stations
      MPRC Manual precipitation sites
      MSNT Manual SNOTEL non-telemetered, non-real time sites
@@ -172,24 +182,31 @@ def site_list(hucs = None, networkCds=None):
      USGS Any USGS station, but also other non-USGS streamflow stations
     '''
     filestring = 'cache/site_list_%s.py' % hucs.replace(':', '-')
+
     def write_contents_to_file():
         f = open(filestring, 'w+')
-        sites = server.getStations(hucs=str(hucs), networkCds=networkCds, logicalAnd='true')
+        sites = server.getStations(
+            hucs=str(hucs),
+            networkCds=networkCds,
+            logicalAnd='true')
         pickle.dump(sites, f)
         f.close()
-    
+
     if checkfile(filestring):
         write_contents_to_file()
     f = open(filestring, 'r')
     return pickle.load(f)
 
+
 def station_meta(site=None):
     '''
-     Gets the beginDate, endDate, fipsCountryCd (US), countyName, fipsStateNumber,
-     elevation, huc (eg. 180102040103), hud (eg. 18020001), latitude, longitude, name,
-     shefId (eg. CRWC1), stationDataTimeZone (eg. -8.0), stationTriplet
+     Gets the beginDate, endDate, fipsCountryCd (US), countyName,
+     fipsStateNumber, elevation, huc (eg. 180102040103), hud (eg. 18020001),
+     latitude, longitude, name, shefId (eg. CRWC1),
+     stationDataTimeZone (eg. -8.0), stationTriplet
     '''
     filestring = 'cache/station_meta_%s.py' % site.replace(':', '-')
+
     def write_contents_to_file():
         f = open(filestring, 'w+')
         station = server.getStationMetadata(stationTriplet=site)
@@ -200,13 +217,15 @@ def station_meta(site=None):
     f = open(filestring)
     return pickle.load(f)
 
-def station_elements(station = None):
+
+def station_elements(station=None):
     '''
      This returns a list of element codes measured at a given station
      Also has the beginDate and endDate and the storedUnitCode for the
      thing being measured.
     '''
     filestring = 'cache/station_elements_%s.py' % station.replace(':', '-')
+
     def write_contents_to_file():
         f = open(filestring, 'w+')
         elements = server.getStationElements(stationTriplet=str(station))
@@ -218,7 +237,7 @@ def station_elements(station = None):
     return pickle.load(f)
 
 
-def element_list(element = None):  # Not needed
+def element_list(element=None):  # Not needed
     '''
      This returns a list of elements, which are measurement codes
      the elementCd needed for queries is accessed at elements[i].elementCd
@@ -228,14 +247,17 @@ def element_list(element = None):  # Not needed
     elements = server.getElements()
     return elements
 
-def get_daily_data(stationTriplets = None, elementCd = None, startDate=None):
+
+def get_daily_data(stationTriplets=None, elementCd=None, startDate=None):
     '''
      This gets the daily data for any site or list of sites
      for only one element code. Although it is possible that other measurements
      can be read from by the ordinal, '1' is the most common according to the
-     documentation. 
+     documentation.
     '''
-    filestring = 'cache/daily_data_%s_%s.py' % (stationTriplets.replace(':', '-'), elementCd)
+    filestring = '''cache/daily_data_%s_%s.py
+                        ''' % (stationTriplets.replace(':', '-'), elementCd)
+
     def write_contents_to_file(**kwargs):
         today = datetime.strftime(datetime.now(), 'YYYY-MM-dd')
         f = open(filestring, 'w+')
@@ -244,7 +266,7 @@ def get_daily_data(stationTriplets = None, elementCd = None, startDate=None):
             elementCd=elementCd,
             ordinal=1,
             duration='DAILY',
-            getFlags='true',  # Needs to be true for the other to be false...goofy!
+            getFlags='true',  # Needs to be true for the other to be false
             beginDate=startDate,
             endDate=str(datetime.date(datetime.now()).isoformat()),
             alwaysReturnDailyFeb29='false'
@@ -256,26 +278,28 @@ def get_daily_data(stationTriplets = None, elementCd = None, startDate=None):
         write_contents_to_file()
     f = open(filestring, 'r')
     return pickle.load(f)
-        
 
-def get_hourly_data(stationTriplets = None, elementCd = None, startDate = None):
+
+def get_hourly_data(stationTriplets=None, elementCd=None, startDate=None):
     '''
      Nearly identical to daily data. Gets hourly values instead
     '''
-    filestring = 'cache/hourly_data_%s_%s.py' % (stationTriplets.replace(':', '-'), elementCd)
+    filestring = '''cache/hourly_data_%s_%s.py
+                    ''' % (stationTriplets.replace(':', '-'), elementCd)
+
     def write_contents_to_file():
         today = datetime.strftime(datetime.now(), 'YYYY-MM-dd')
         f = open(filestring, 'w+')
         data = server.getHourlyData(
-            stationTriplets = stationTriplets,
-            elementCd = elementCd,
-            ordinal = 1,
-            beginDate = startDate,
-            endDate = str(datetime.date(datetime.now()).isoformat()),
+            stationTriplets=stationTriplets,
+            elementCd=elementCd,
+            ordinal=1,
+            beginDate=startDate,
+            endDate=str(datetime.date(datetime.now()).isoformat()),
         )
         pickle.dump(data, f)
         f.close()
-        
+
     if checkfile(filestring):
         write_contents_to_file()
     f = open(filestring, 'r')
