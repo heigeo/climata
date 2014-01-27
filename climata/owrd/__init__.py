1df1920fc6f37c9f9aa9ee1ed7e69e78ac1ff47b
Jeremiah Sandahl <jeremiahsa@gmail.com>
2014-01-27 10:27:33 -0600

    cleaned up formatting for the owrd IO.

--- climata/owrd/__init__.py
+++ climata/owrd/__init__.py
@@ -1,27 +1,27 @@
 from wq.io import CsvNetIO
 from datetime import datetime
 
+
 class OwrdWaterIO(CsvNetIO):
     delimiter = '\t'
     debug = True
     max_header_row = 15
     filename = ''
-    
+    burl = 'http://filepickup.wrd.state.or.us/files/Publications/obswells/data'
+
     @property
     def url(self):
-        return 'http://filepickup.wrd.state.or.us/files/Publications/obswells/data/%s' % self.filename
-    
-    
-    
+        return '%s/%s' % (self.burl, self.filename)
+
     '''
     @property
     def filename(self):
         return self.base_url + self.text_file
-    
         # water_level_data_url = base_url + 'owrd_wls.txt'
         # well_data_url = base_url + 'owrd_master.txt'
         # owrd_redrils_url = base_url + 'owrd_redrills.txt'
     '''
+
 
 class OwrdFlowIO(CsvNetIO):
     delimiter = '\t'
@@ -29,11 +29,13 @@ class OwrdFlowIO(CsvNetIO):
     station_nbr = ''
     dataset = ''
     start_date = ''
-    
+    burl1 = 'http://apps.wrd.state.or.us/apps/sw/hydro_near_real_time/'
+    burl2 = 'hydro_download.aspx'
+
     @property
     def url(self):
-        return 'http://apps.wrd.state.or.us/apps/sw/hydro_near_real_time/hydro_download.aspx' % self.params
-    
+        return '%s%s' (self.burl1, self.burl2) % self.params
+
     @property
     def params(self):
         return {
