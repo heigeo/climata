05344e81c693560001d546c937e90b5353706c7c
Jeremiah Sandahl <jeremiahsa@gmail.com>
2014-02-10 11:56:30 -0600

    Added a forecast IO for getting forecasts from the CA/NV weather service HTML.

--- climata/nws/__init__.py
+++ climata/nws/__init__.py
@@ -54,14 +54,14 @@ class NWSIO(XmlNetIO):
             self.root_tag = root.tag
         if self.item_tag is None:
             self.item_tag = list(root)[1].tag # This is the data
-        self.data = map(self.parse_item, root.findall(self.item_tag))
+        self.data = self.parse_item(list(root)[1])
+        #self.data = map(self.parse_item, root.findall(self.item_tag))
     
     def parse_item(self, element):
-        item = {}
+        item = []
         for elem in element:
             layout_key = ''
             if elem.tag == 'time-layout':
-                print elem[0].text
                 start_dates = []
                 end_dates = []
                 for el in elem:
@@ -71,10 +71,7 @@ class NWSIO(XmlNetIO):
                         start_dates.append(el.text)
                     elif el.tag == 'end-valid-time':
                         end_dates.append(el.text)
-                item[layout_key] = {'start_dates':[], 'end_dates':[], 'name': '', 'values':[]}
-                item[layout_key]['start_dates'] = start_dates
-                item[layout_key]['end_dates'] = end_dates
-                
+                item.append({'key':layout_key, 'start_dates':start_dates, 'end_dates':end_dates, 'name': '', 'values':[]})
             if elem.tag == 'parameters':
                 for el in elem: # This level is a climate-anomaly
                     for e in el: # This level is where the tag gets the time-layout           
@@ -82,8 +79,114 @@ class NWSIO(XmlNetIO):
                         values_list = []
                         for val in e:
                             if val.tag == 'name':
-                                item[layout_key]['name'] = val.text
+                                for it in item:
+                                    if it['key'] == layout_key:
+                                        it['name'] = val.text
                             else:
                                 values_list.append(val.text)
-                        item[layout_key]['values'] = values_list
-        return item
+                        for it in item:
+                            if it['key'] == layout_key:
+                                it['values'] = values_list
+        item_to_return = []
+        for a, b in enumerate(item):
+            item_to_return.append({'key': b['key'], 'name': b['name'], 'values': []})
+            values_as_timeseries = []
+            for j, k in enumerate(b['end_dates']):
+                ts = {
+                    'end_date': b['end_dates'][j], 
+                    'start_date': b['start_dates'][j],
+                    'value': b['values'][j],
+                    }
+                values_as_timeseries.append(ts)
+            item_to_return[a]['values']=values_as_timeseries
+        return item_to_return
+
+
+class NWSForecastIO(XmlNetIO):
+    ###########################################
+    # valid is the time of the forecast in UTC
+    # primary is the stage in ft 
+    # secondary is the flow in kcfs
+    # pedts is ???
+    # It appears to return 3 days forecast in the future
+    # 
+    ###########################################
+    gage = ''
+
+    @property
+    def url(self):
+        return 'http://water.weather.gov/ahps2/hydrograph_to_xml.php' % self.params
+
+    @property
+    def params(self):
+        return {
+            'gage':self.gage,
+            'output':'xml',
+        }
+
+    def parse(self):
+        doc = ET.parse(self.file)
+        root = doc.getroot()
+        if self.root_tag is None:
+            self.root_tag = root.tag
+        if self.item_tag is None:
+            for l in list(root):
+                if l.tag == 'forecast':
+                    self.item_tag = l
+                    print self.item_tag
+        self.data = self.parse_item(self.item_tag)
+
+    def parse_item(self, element):
+        items = []
+        for el in element:
+            item = {
+                el[0].tag : el[0].text,
+                el[1].tag : el[1].text,
+                el[2].tag : el[2].text,
+                el[3].tag : el[3].text,
+            }
+            items.append(item)
+        return items
+
+
+class CNRFForecastIO(CSVNetIO):
+    ###################################################
+    # This gets the table in tab-separated HTML format
+    # Hopefully we can get a better way to access the
+    # forecast data.
+    ###################################################
+    location = 'KLAO3'
+    accumtype = 'mean'
+    interval = 'day'
+    disttype = 'empirical'
+    S_month = '02'
+    S_day = '10'
+    S_year = '2014'
+    E_month = '05'
+    E_day= '10'
+    E_year= '2014'
+    plottype = 'traces'
+    tabletype = 'forecastinfo'
+    outtype = 'Generate+a+Table'
+    
+    @property
+    def url(self):
+        return 'http://www.cnrfc.noaa.gov/send_espTrace.cgi' % self.params
+
+    @property
+    def params(self):
+        return {
+            'location':self.location,
+            'accumtype':self.accumtype,
+            'interval':self.interval,
+            'disttype':self.disttype,
+            'S_month':self.S_month,
+            'S_day':self.S_day,
+            'S_year':self.S_year,
+            'E_month':self.E_month,
+            'E_day':self.E_day,
+            'E_year':self.E_year,
+            'plottype':self.plottype,
+            'tabletype':self.tabletype,
+            'outtype':self.outtype,
+        }
